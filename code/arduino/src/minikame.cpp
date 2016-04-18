183340f86cf44fad4d732e49aca7fdc9935fe521
javacasm <javacasm@gmail.com>
2016-04-18 11:50:22 +0200

    pinout

--- code/arduino/src/minikame.cpp
+++ code/arduino/src/minikame.cpp
@@ -6,14 +6,14 @@ int angToUsec(float value){
 
 void MiniKame::init(){
 
-    board_pins[0] = D1;
-    board_pins[1] = D4,
-    board_pins[2] = D8;
-    board_pins[3] = D6;
-    board_pins[4] = D7;
-    board_pins[5] = D5;
-    board_pins[6] = D2;
-    board_pins[7] = D3;
+    board_pins[0] = D1; // front left inner
+    board_pins[1] = D4, // front right inner
+    board_pins[2] = D8; // front left outer
+    board_pins[3] = D6; // front right outer
+    board_pins[4] = D7; // back left inner
+    board_pins[5] = D5; // back right inner
+    board_pins[6] = D2; // back left outer
+    board_pins[7] = D3; // back right outer
 
     trim[0] = 1;
     trim[1] = -8;
